7e0af7230b277f14bbf6c1c821bf0c1ce490bcfc
Bob Rusch <git-commit-address>
2021-03-28 11:48:41 -0400

    BASE - empty app

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text, AppRegistry} from 'react-native';
+
+const App = () => {
+  return <Text>SOME TEXT</Text>;
+};
+
+AppRegistry.registerComponent('albums', () => App);
